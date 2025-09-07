f7c948f5d420c959548883ea027efab4eb1dc950
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2025-09-07 23:52:17 +0530

    refactor: rename fn to throttle

--- Javascript/conceptual/rate-limit/Throttle.js
+++ Javascript/conceptual/rate-limit/Throttle.js
@@ -1,11 +1,11 @@
 /*
 📌 Problem:
-Implement a function `rateLimiter(fn, limit)` that returns a new function
+Implement a function `throttle(fn, limit)` that returns a new function
 which allows calling `fn` only once every `limit` milliseconds.
 
 -------------------------------------------------
 🔹 Example:
-const limitedLog = rateLimiter(console.log, 1000);
+const limitedLog = throttle(console.log, 1000);
 limitedLog("Hello"); // executes immediately
 limitedLog("World"); // ignored if called within 1s
 After 1s → next call works again.
@@ -16,7 +16,7 @@ After 1s → next call works again.
 - Space: O(1) (store last execution timestamp)
 */
 
-function rateLimiter(fn, limit) {
+function throttle(fn, limit) {
   let lastExecuted = 0; // closure variable
 
   return function (...args) {
@@ -31,7 +31,7 @@ function rateLimiter(fn, limit) {
 }
 
 // ✅ Example Usage
-const limitedLog = rateLimiter((msg) => console.log(msg, Date.now()), 1000);
+const limitedLog = throttle((msg) => console.log(msg, Date.now()), 1000);
 
 limitedLog("First"); // executes immediately
 setTimeout(() => limitedLog("Second"), 500); // ignored (too soon)
